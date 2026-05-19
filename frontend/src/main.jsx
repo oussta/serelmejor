eec66233b2c1204e7135ca4cf5b14fec8fabdabf
oussta <ousstatada@gmail.com>
2026-05-19 21:41:01 +0200

    fix: payment flow + order stock logic + role guard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>
 )
